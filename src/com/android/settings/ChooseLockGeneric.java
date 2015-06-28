a41a63211a0bab749b892d2a15d56af9938525c5
Daniel Micay <danielmicay@gmail.com>
2015-06-28 13:31:54 -0400

    fix usage of ChooseLockSettingsHelper
    The fragment was not being passed, so the ChooseLock* fragments are not
    informed if authentication fails (i.e. the cancel button is pushed) and
    continue on as if it was successful. This wasn't noticed because this
    code isn't used in the normal path where authentication happens before
    entering the preferences menu to choose the authentication method.
    
    The authentication support built-in to these fragments may even be dead
    code, but it should work correctly if it's there because it may be the
    basis of new code.
    
    Change-Id: I4397c6e6a05cf34dbac066bf0548847da6bf0561

--- src/com/android/settings/ChooseLockGeneric.java
+++ src/com/android/settings/ChooseLockGeneric.java
@@ -146,7 +146,7 @@ public class ChooseLockGeneric extends SettingsActivity {
                 (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
             mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
             mKeyStore = KeyStore.getInstance();
-            mChooseLockSettingsHelper = new ChooseLockSettingsHelper(this.getActivity());
+            mChooseLockSettingsHelper = new ChooseLockSettingsHelper(this.getActivity(), this);
             mLockPatternUtils = new LockPatternUtils(getActivity());
             mIsSetNewPassword = ACTION_SET_NEW_PARENT_PROFILE_PASSWORD.equals(chooseLockAction)
                     || ACTION_SET_NEW_PASSWORD.equals(chooseLockAction);

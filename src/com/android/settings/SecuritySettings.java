348b72c6d601fb6ef2dda9542cd4dd7fa5a33948
Daniel Micay <danielmicay@gmail.com>
2016-10-15 14:13:10 -0400

    add toggle for background clipboard access
    Change-Id: If66cec9255539429ae7202f515cbddd8ff54ab1f

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -119,10 +119,13 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String DENY_NEW_USB_PROP = "security.deny_new_usb";
     private static final String DENY_NEW_USB_PERSIST_PROP = "persist.security.deny_new_usb";
 
+    private static final String KEY_BACKGROUND_CLIPBOARD = "background_clipboard";
+    private static final String BACKGROUND_CLIPBOARD_PERSIST_PROP = "persist.security.bg_clipboard";
+
     // These switch preferences need special handling since they're not all stored in Settings.
     private static final String SWITCH_PREFERENCE_KEYS[] = {
             KEY_SHOW_PASSWORD, KEY_TOGGLE_INSTALL_APPLICATIONS, KEY_UNIFICATION,
-            KEY_VISIBLE_PATTERN_PROFILE, KEY_DENY_NEW_USB
+            KEY_VISIBLE_PATTERN_PROFILE, KEY_DENY_NEW_USB, KEY_BACKGROUND_CLIPBOARD
     };
 
     // Only allow one trust agent on the platform.
@@ -156,6 +159,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private int mProfileChallengeUserId;
 
     private ListPreference mDenyNewUsb;
+    private SwitchPreference mBackgroundClipboard;
 
     private String mCurrentDevicePassword;
     private String mCurrentProfilePassword;
@@ -410,6 +414,14 @@ public class SecuritySettings extends SettingsPreferenceFragment
             }
         }
 
+        if (mIsAdmin) {
+            mBackgroundClipboard = (SwitchPreference) findPreference(KEY_BACKGROUND_CLIPBOARD);
+        } else {
+            if (advancedCategory != null) {
+                advancedCategory.removePreference(advancedCategory.findPreference(KEY_BACKGROUND_CLIPBOARD));
+            }
+        }
+
         // The above preferences come and go based on security state, so we need to update
         // the index. This call is expected to be fairly cheap, but we may want to do something
         // smarter in the future.
@@ -631,6 +643,10 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (mDenyNewUsb != null) {
             mDenyNewUsb.setValue(SystemProperties.get(DENY_NEW_USB_PERSIST_PROP, "disabled"));
         }
+
+        if (mBackgroundClipboard != null) {
+            mBackgroundClipboard.setChecked(SystemProperties.getBoolean(BACKGROUND_CLIPBOARD_PERSIST_PROP, false));
+        }
     }
 
     private void updateUnificationPreference() {
@@ -821,6 +837,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
             if (mode.equals("dynamic")) {
                 SystemProperties.set(DENY_NEW_USB_PROP, "0");
             }
+        } else if (KEY_BACKGROUND_CLIPBOARD.equals(key)) {
+            boolean enabled = (Boolean) value;
+            SystemProperties.set(BACKGROUND_CLIPBOARD_PERSIST_PROP, enabled ? "1" : "0");
         }
         return result;
     }
